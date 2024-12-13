1202e4b53f0be6ff41bf23ffc0c415b704f6ec9d
UnTizioCheEsiste <nicolociancaglia@gmail.com>
2024-12-13 15:54:54 +0100

    Added JSON support for saving on files.

--- src/main/java/com/untizio/App.java
+++ src/main/java/com/untizio/App.java
@@ -1,7 +1,14 @@
 package com.untizio;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.prefs.Preferences;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.untizio.controller.StudentEditDialogController;
 import com.untizio.controller.StudentOverviewController;
 import com.untizio.model.Student;
@@ -11,6 +18,8 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.DialogPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
@@ -123,4 +132,73 @@ public class App extends Application {
         }
     }
 
+    /**
+    * Returnsthe person file preference, i.e. the file that waslast opened.
+    * preference can be found, null is returned.
+    * The preference is read from the OS specific registry. If no such
+    *
+    * @return
+    */
+    public File getStudentFilePath() {
+        Preferences prefs = Preferences.userNodeForPackage(App.class);
+        String filePath = prefs.get("filePath", null);
+        if (filePath != null) {
+            return new File(filePath);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+    * Sets the file path of the currently loaded file. The path is persisted in
+    * the OS specific registry.
+    *
+    * @param file the file or null to remove the path
+    */
+    public void setStudentFilePath(File file) {
+        Preferences prefs = Preferences.userNodeForPackage(App.class);
+        if (file != null) {
+            prefs.put("filePath", file.getPath());
+            // Update the stage title.
+            primaryStage.setTitle("Scuola - " + file.getName());
+        } else {
+                prefs.remove("filePath");
+                // Update the stage title.
+                primaryStage.setTitle("Scuola");
+        }
+    }
+
+    public void loadStudentDataFromFile(File file) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.registerModule(new JavaTimeModule());
+            studentData.setAll(
+                FXCollections.observableArrayList(mapper.readValue(file, new TypeReference<List<Student>>() {}))
+            );
+            setStudentFilePath(file);
+        } catch (Exception e) { // catches ANY exception
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Could not load data");
+            alert.setContentText("Could not load data from file:\n" + file.getPath());
+            alert.showAndWait();
+        }
+    }
+    
+    public void saveStudentDataToFile(File file) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+            mapper.registerModule(new JavaTimeModule());
+            mapper.writeValue(file, studentData);
+            // Save the file path to the registry.
+            setStudentFilePath(file);
+        } catch (Exception e) { // catches ANY exception
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Could not save data");
+            alert.setContentText("Could not save data to file:\n" + file.getPath());
+            alert.showAndWait();
+        }
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module com.untizio {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.base;
+    requires java.prefs;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
 
     opens com.untizio to javafx.fxml;
     opens com.untizio.controller to javafx.fxml;
